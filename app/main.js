0081bc881bdf13aeaa6a5ddbec4bba4dd0f1ca03
katsuyagoto <katsuya.goto.dev@gmail.com>
2018-04-26 19:37:27 +0900

    Fix menu

--- app/main.js
+++ app/main.js
@@ -94,4 +94,36 @@ app.on("ready", async () => {
       ]).popup(mainWindow);
     });
   }
+
+  const template = [
+    {
+      label: "daily",
+      submenu: [
+        {
+          label: "Quit",
+          accelerator: "Command+Q",
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:",
+        },
+      ],
+    },
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
